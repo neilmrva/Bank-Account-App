928c42be72f10b4b61819eb9c3f686518d742cf4
Neil Mrva <neil.mrva@gmail.com>
2018-12-21 21:43:30 -0500

    - Add state controller that adds and updates a list of accounts and saves to file

---
+++ Bank Account App/Bank Account App/Model Controllers/StateController.swift
@@ -0,0 +1,41 @@
+//
+//  StateController.swift
+//  Bank Account App
+//
+//  Created by Work on 12/21/18.
+//  Copyright © 2018 neilmrva. All rights reserved.
+//
+
+import Foundation
+
+class StateController
+{
+    private let storageController:StorageController
+    private var accounts:[Account]
+    
+    init(storageController:StorageController)
+    {
+        self.storageController = storageController
+        self.accounts = storageController.loadAccounts() ?? [Account]()
+    }
+    
+    func add(account:Account)
+    {
+        accounts.append(account)
+        storageController.save(accounts: accounts)
+    }
+    
+    func update(account:Account)
+    {
+        for (index, storedAccount) in accounts.enumerated()
+        {
+            if storedAccount.number == account.number
+            {
+                accounts[index] = account
+                storageController.save(accounts: accounts)
+                break
+            }
+        }
+    }
+    
+}
