711ea3fe8830c0a78df0235afd1189f837d2ee5d
Neil Mrva <neil.mrva@gmail.com>
2018-12-22 20:53:18 -0500

    - Add custom view controller classes for all scenes in the storyboard - Add unwind segue functions to account and transaction view controllers for the modal creation screens to exit from

---
+++ Bank Account App/Bank Account App/View Controllers/CreateTransactionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateTransactionViewController.swift
+//  Bank Account App
+//
+//  Created by Work on 12/22/18.
+//  Copyright © 2018 neilmrva. All rights reserved.
+//
+
+import UIKit
+
+class CreateTransactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bank Account App/Bank Account App/View Controllers/TransactionsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TransactionsViewController.swift
+//  Bank Account App
+//
+//  Created by Work on 12/22/18.
+//  Copyright © 2018 neilmrva. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsViewController:UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelTransactionCreation(_ segue:UIStoryboardSegue)
+    {
+        
+    }
+
+    @IBAction func saveTransaction(_ segue:UIStoryboardSegue)
+    {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
