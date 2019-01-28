9c495512e4fa62ac8c567edbdd9651ce338b4330
Neil Mrva <neil.mrva@gmail.com>
2019-01-28 11:43:05 -0500

    - Add computed properties, selection and rest function to Transaction View

--- Bank Account App/Bank Account App/Views/NewTransactionView.swift
+++ Bank Account App/Bank Account App/Views/NewTransactionView.swift
@@ -11,18 +11,78 @@ import UIKit
 
 class NewTransactionView:UIView
 {
+    // MARK: Private Outlets
     @IBOutlet fileprivate weak var amountTextField: UITextField!
     @IBOutlet fileprivate weak var descriptionTextField: UITextField!
     @IBOutlet fileprivate var ringViews:[UIView]!
     @IBOutlet fileprivate var categoryButtons:[UIButton]!
     
+    // MARK: Private Properties
     fileprivate var selectedIndex:Int = 0
+    
+    // MARK: Public Computed Properties
+    var amount:Float?
+    {
+        get
+        {
+            guard let text = amountTextField.text
+            else
+            {
+                return nil
+            }
+            
+            return Float(text)
+        }
+    }
+    
+    var transactionDescription:String?
+    {
+        get
+        {
+            return descriptionTextField.text
+        }
+    }
+    
+    var selectedCategory:TransactionCategory?
+    {
+        get
+        {
+            switch selectedIndex
+            {
+                case 0: return .income
+                case 1: return .groceries
+                case 2: return .utilities
+                case 3: return .home
+                case 4: return .transport
+                case 5: return .fun
+                default: return nil
+            }
+        }
+    }
     
     @IBAction func onCategorySelected(_ sender: UIButton)
     {
+        // reset previous ring
+        resetRings()
+        
+        // get selected ring index
         selectedIndex = categoryButtons.index(of: sender)!
-        print(selectedIndex)
+        
+        // set ring color
+        let selectedRingView = ringViews[selectedIndex]
+        selectedRingView.layer.borderColor = UIColor.skyBlue.cgColor
     }
     
+    fileprivate func resetRings()
+    {
+        for ringView in ringViews
+        {
+            ringView.layer.borderColor = UIColor.clear.cgColor
+        }
+    }
     
+    override func awakeFromNib()
+    {
+        resetRings()
+    }
 }
